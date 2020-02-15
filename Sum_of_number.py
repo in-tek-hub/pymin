25a70f49dc321f9f90602ff132b8d3d36664a531
Intekhab Choudhury <Intekhab.Choudhury@travelport.com>
2020-02-15 01:37:21 -0500

    Fibonacci Sequence Initial

---
+++ Sum_of_number.py
@@ -0,0 +1,14 @@
+# sum of all numbers stored in a list
+
+# List
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+	sum = sum+val
+
+# Output: The sum is 48
+print("The sum is", sum)
